4528aafb8f14ce227a0d0e8b2f830353f1354d08
Abhisek Das <abhisekdas@Abhiseks-MacBook-Pro.local>
2024-10-10 00:28:59 +0530

    added suspense

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { trpc } from '../_trpc/client';
 import { Loader2 } from 'lucide-react';
 
@@ -30,13 +30,15 @@ const Page = () => {
   
   
   return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800'/>
-        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
-        <p>You will be redirected automatically.</p>
+    <Suspense fallback={<Loader2 className='h-8 w-8 animate-spin text-zinc-800'/>}>
+      <div className='w-full mt-24 flex justify-center'>
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className='h-8 w-8 animate-spin text-zinc-800'/>
+          <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 
 }
