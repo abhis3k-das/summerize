ce7b5b5e9d02c1a1d11a3632cc4b76ccc8bffed4
Abhisek Das <abhisekdas@Abhiseks-MacBook-Pro.local>
2024-10-10 00:45:45 +0530

    id fix

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,19 +1,17 @@
 "use client"
-import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Suspense, useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { Suspense, useEffect } from 'react';
 import { trpc } from '../_trpc/client';
 import { Loader2 } from 'lucide-react';
 
-const Page = () => {
+const PageContent = () => {
   const router = useRouter();
-  
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { data, isLoading, error } = trpc.authCallback.useQuery(undefined,{
-    retry:true,
-    retryDelay : 500,
+  const { data, isLoading, error } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
   });
 
   useEffect(() => {
@@ -21,26 +19,30 @@ const Page = () => {
       router.push(origin ? `/${origin}` : '/dashboard');
     }
   }, [data, router, origin]);
-  
+
   useEffect(() => {
     if (error?.data?.code === 'UNAUTHORIZED') {
       router.push("/sign-in");
     }
-  }, [error,router]);
-  
-  
-  return (
-    <Suspense fallback={<Loader2 className='h-8 w-8 animate-spin text-zinc-800'/>}>
-      <div className='w-full mt-24 flex justify-center'>
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className='h-8 w-8 animate-spin text-zinc-800'/>
-          <h3 className='font-semibold text-xl'>Setting up your account...</h3>
-          <p>You will be redirected automatically.</p>
-        </div>
-      </div>
-    </Suspense>
-  )
+  }, [error, router]);
 
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Page
+const Page = () => {
+  return (
+    <Suspense fallback={<Loader2 className='h-8 w-8 animate-spin text-zinc-800' />}>
+      <PageContent />
+    </Suspense>
+  );
+}
+
+export default Page;
