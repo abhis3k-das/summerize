21fa107d8768d339eb524cf42f45cdfd7d4bbf81
Abhisek Das <abhisekdas@Abhiseks-MacBook-Pro.local>
2024-10-10 00:48:42 +0530

    minor fix

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -9,6 +9,7 @@ const PageContent = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, isLoading, error } = trpc.authCallback.useQuery(undefined, {
     retry: true,
     retryDelay: 500,
