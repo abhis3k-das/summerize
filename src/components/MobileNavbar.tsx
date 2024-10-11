586b898bbd54f41ad8e5075e28773436691a403a
Abhisek Das <abhisekdas@Abhiseks-MacBook-Pro.local>
2024-10-11 16:58:06 +0530

    lint fix

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -4,7 +4,6 @@ import { ArrowRight, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { boolean } from "zod";
 
 interface MobileNavProp {
     isAuth : boolean,
